b306871ee8be783e3e65e31f2df403a2e8d0a3ef
GitHub Version Bumper <github-version-bumper@users.noreply.github.com>
2021-06-28 19:16:24 +0530

    extended bumpping for more branches

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ console.log(`Version ${version}`)
 
 Toolkit.run(async (tools) => {
   const fileName = process.env.VERSION_FILE_NAME || 'package.json'
-  const entry = process.env.VERSION_ENTRY || 'version'
+  let entry = process.env.VERSION_ENTRY || 'version'
   const githubUser = process.env.GITHUB_USER || 'GitHub Version Bumper'
   const githubEmail =
-    process.env.GITHUB_EMAIL || 'github-version-bumper@users.noreply.github.com'
+      process.env.GITHUB_EMAIL || 'github-version-bumper@users.noreply.github.com'
 
   const commitMessage = 'version bumped to v'
 
@@ -40,31 +40,41 @@ Toolkit.run(async (tools) => {
     console.log('lastcommitmessage', lastCommit)
 
     // Bumping Starts
-
-    if (lastCommit.toLowerCase().includes('ci-ignore')) {
-      console.log('ci-ignore')
-      ignoreBump = true
-    } else if (lastCommit.toLowerCase().includes('ci-version=')) {
-      const splitted = lastCommit.toLowerCase().split('ci-version=\\"')
-      const replace = splitted[1].split('\\"')[0]
-      console.log('replace:', replace)
-      await bumpVersion(fileName, { replace, entry })
-    } else if (lastCommit.toLowerCase().includes('ci-pre=')) {
-      console.log('pre')
-      const splitted = lastCommit.toLowerCase().split('ci-pre=\\"')
-      const pre = splitted[1].split('\\"')[0]
-      console.log('pre:', pre)
-      await bumpVersion(fileName, { pre, entry })
-    } else if (lastCommit.toLowerCase().includes('ci-major')) {
-      console.log('major')
-      await bumpVersion(fileName, { major: true, entry })
-    } else if (lastCommit.toLowerCase().includes('ci-minor')) {
-      console.log('minor')
-      await bumpVersion(fileName, { minor: true, entry })
-    } else {
-      console.log('patch')
+    if(currentBranch === 'master'){
+      if (lastCommit.toLowerCase().includes('ci-ignore')) {
+        console.log('ci-ignore')
+        ignoreBump = true
+      } else if (lastCommit.toLowerCase().includes('ci-version=')) {
+        const splitted = lastCommit.toLowerCase().split('ci-version=\\"')
+        const replace = splitted[1].split('\\"')[0]
+        console.log('replace:', replace)
+        await bumpVersion(fileName, { replace, entry })
+      } else if (lastCommit.toLowerCase().includes('ci-pre=')) {
+        console.log('pre')
+        const splitted = lastCommit.toLowerCase().split('ci-pre=\\"')
+        const pre = splitted[1].split('\\"')[0]
+        console.log('pre:', pre)
+        await bumpVersion(fileName, { pre, entry })
+      } else if (lastCommit.toLowerCase().includes('ci-major')) {
+        console.log('major')
+        await bumpVersion(fileName, { major: true, entry })
+      } else if (lastCommit.toLowerCase().includes('ci-minor')) {
+        console.log('minor')
+        await bumpVersion(fileName, { minor: true, entry })
+      } else {
+        console.log('patch')
+        await bumpVersion(fileName)
+      }
+    }
+    else if(currentBranch === 'staging' || currentBranch === 'qc'  || currentBranch === 'production' ){
+      console.log('current branch')
       await bumpVersion(fileName)
     }
+    else if(currentBranch === 'alpha'){
+
+    }
+
+
 
     if (!ignoreBump) {
       const newVersion = JSON.parse(tools.getFile(fileName)).version
