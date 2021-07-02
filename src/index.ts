e45d5cb33d4b53549ecbed92c5734096ea8e2cf6
GitHub Version Bumper <github-version-bumper@users.noreply.github.com>
2021-07-02 16:46:48 +0530

    extended bumpping for more branches

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,7 @@ Toolkit.run(async (tools) => {
     }
     else if(currentBranch === 'staging' || currentBranch === 'qc'  || currentBranch === 'production' ){
       console.log('current branch')
-      await bumpVersion(fileName)
+      await bumpVersion(fileName,{ major: 1 })
     }
     else if(currentBranch === 'alpha'){
 
