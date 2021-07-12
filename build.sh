b24dbede8288c7c5d19cd0f5081a0cbab9778069
Jeremy R DeYoung <jeremy@nextstep.guru>
2021-07-12 10:30:42 -0500

    reworked build to auto-bump the local version as well

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 #
 export CURRENT_VERSION=$(cat version.json | jq -r ".version")
 CURRENT_VERSION=`npx semver "$CURRENT_VERSION" -i`
-echo "{\"version\": \"$CURRENT_VERSION\"}" > version.json;
+echo "testing...$CURRENT_VERSION"
+echo "`jq '.version=\"'$CURRENT_VERSION'\"' version.json`" > version.json
 export CURRENT_VERSION=$(cat version.json | jq -r ".version")
-echo "`jq '.version="`$CURRENT_VERSION`"' package.json`" > package.json
+echo "`jq '.version=\"'$CURRENT_VERSION'\"' package.json`" > package.json
 parcel build ./src/index.ts --no-source-maps --target node
