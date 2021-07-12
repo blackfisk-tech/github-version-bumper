909da295eb934d04662f8ca847d29e3d115f2798
Jeremy R DeYoung <jeremy@nextstep.guru>
2021-07-12 09:42:56 -0500

    .

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ console.log(`Version ${version}`)
 
 Toolkit.run(async (tools) => {
   const fileName = process.env.VERSION_FILE_NAME || 'package.json'
-  let entry = process.env.VERSION_ENTRY || 'version'
+  const entry = process.env.VERSION_ENTRY || 'version'
   const githubUser = process.env.GITHUB_USER || 'GitHub Version Bumper'
   const githubEmail =
       process.env.GITHUB_EMAIL || 'github-version-bumper@users.noreply.github.com'
@@ -40,30 +40,30 @@ Toolkit.run(async (tools) => {
     console.log('lastcommitmessage', lastCommit)
 
     // Bumping Starts
-      if (lastCommit.toLowerCase().includes('ci-ignore')) {
-        console.log('ci-ignore')
-        ignoreBump = true
-      } else if (lastCommit.toLowerCase().includes('ci-version=')) {
-        const splitted = lastCommit.toLowerCase().split('ci-version=\\"')
-        const replace = splitted[1].split('\\"')[0]
-        console.log('replace:', replace)
-        await bumpVersion(fileName, { replace, entry })
-      } else if (lastCommit.toLowerCase().includes('ci-pre=')) {
-        console.log('pre')
-        const splitted = lastCommit.toLowerCase().split('ci-pre=\\"')
-        const pre = splitted[1].split('\\"')[0]
-        console.log('pre:', pre)
-        await bumpVersion(fileName, { pre, entry })
-      } else if (lastCommit.toLowerCase().includes('ci-major')) {
-        console.log('major')
-        await bumpVersion(fileName, { major: true, entry })
-      } else if (lastCommit.toLowerCase().includes('ci-minor')) {
-        console.log('minor')
-        await bumpVersion(fileName, { minor: true, entry })
-      } else {
-        console.log('patch')
-        await bumpVersion(fileName)
-      }
+    if (lastCommit.toLowerCase().includes('ci-ignore')) {
+      console.log('ci-ignore')
+      ignoreBump = true
+    } else if (lastCommit.toLowerCase().includes('ci-version=')) {
+      const splitted = lastCommit.toLowerCase().split('ci-version=\\"')
+      const replace = splitted[1].split('\\"')[0]
+      console.log('replace:', replace)
+      await bumpVersion(fileName, { replace, entry })
+    } else if (lastCommit.toLowerCase().includes('ci-pre=')) {
+      console.log('pre')
+      const splitted = lastCommit.toLowerCase().split('ci-pre=\\"')
+      const pre = splitted[1].split('\\"')[0]
+      console.log('pre:', pre)
+      await bumpVersion(fileName, { pre, entry })
+    } else if (lastCommit.toLowerCase().includes('ci-major')) {
+      console.log('major')
+      await bumpVersion(fileName, { major: true, entry })
+    } else if (lastCommit.toLowerCase().includes('ci-minor')) {
+      console.log('minor')
+      await bumpVersion(fileName, { minor: true, entry })
+    } else {
+      console.log('patch')
+      await bumpVersion(fileName)
+    }
 
     if (!ignoreBump) {
       const newVersion = JSON.parse(tools.getFile(fileName)).version
